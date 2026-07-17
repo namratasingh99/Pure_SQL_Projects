92c38b8d20586cf98deeccf9f807659f0efcd90a
namratasingh99 <218240600+namratasingh99@users.noreply.github.com>
2026-07-17 22:09:32 +0530

    Insert author data into authors table

---
+++ Library_Management_Database/insert_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO authors
+ (author_id , author_name)
+ VALUES
+ (1 , "Ruskin Bond"),
+ (2 , "William Shakespeare"),
+ (3 , "Premchand"),
+ (4,"Rabindranath Tagore"),
+ (5 , "RK Narayan");
